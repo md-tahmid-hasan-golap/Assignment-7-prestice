b8bcb8141986acdba4e908edc412d8dd35d6befe
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-17 15:33:43 +0600

    add banner

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: "url(https://i.ibb.co/LzCGQFf3/Banner-min.jpg)",
+        }}
+      >
+        <div className="hero-overlay"></div>
+        <div className="hero-content text-neutral-content text-center">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-3xl font-bold">
+              Bid on Unique Items from <br /> Around the World
+            </h1>
+            <p className="mb-5 text-gray-300">
+              Discover rare collectibles, luxury goods, and vintage <br />{" "}
+              treasures in our curated auctions
+            </p>
+            <button className="btn btn-primary">Explore Auctions</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;

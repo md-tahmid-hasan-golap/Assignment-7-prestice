b8c7fe32b54ea76293fbc3f13df6e0ee9e6d4d6f
md-tahmid-hasan-golap <golapraj47@gmail.com>
2025-05-18 13:55:26 +0600

    resolve

--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -1,21 +1,26 @@
 import React from "react";
+import { RxCrossCircled } from "react-icons/rx";
 
 const Text = ({ marked }) => {
   return (
-    <div>
-      <div className="flex items-center pt-3 space-x-2">
-        <img
-          className="mask mask-squircle h-12 w-12"
-          src={marked.image}
-          alt=""
-        />
-        <h2 className="text-xl">{marked.title}</h2>
+    <div className="flex">
+      <div>
+        <div className="flex items-center pt-3 space-x-2 mt-5">
+          <img
+            className="mask mask-squircle h-12 w-12"
+            src={marked.image}
+            alt=""
+          />
+          <h2 className="text-xl">{marked.title}</h2>
+        </div>
+        <div className="flex justify-evenly">
+          <p>${marked.currentBidPrice}</p>
+          <p> Bids :{marked.bidsCount}</p>
+        </div>
       </div>
-
-      <div className="flex justify-evenly">
-        <p>${marked.currentBidPrice}</p>
-        <p> Bids :{marked.bidsCount}</p>
-      </div>
+      <button>
+        <RxCrossCircled size={25} />
+      </button>
     </div>
   );
 };
